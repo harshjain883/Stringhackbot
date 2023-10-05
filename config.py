60a749ba59c4b424752ee6ea3d6643373a3a3cbb
Arpit Singh <137784719+DAXXSIR@users.noreply.github.com>
2023-10-05 13:18:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     API_ID = "25450075"
     API_HASH = "278e22b00d6dd565c837405eda49e6f2"
-    TOKEN = "6521122303:AAGs0DC2DroQjTF2gMcvtQ3Exgtjlp3xEtM"
+    TOKEN = "6521122303:AAFgqGJkP-Sj-CzRw11Xf4vHWBMbdJIyZqQ"
     START_PIC = "https://telegra.ph/file/2dcb2ea8da198a5507df7.jpg"
